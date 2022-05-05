cd0ac2330720c1cbda6d9cc077d475f49391a8a1
Jay Lee <dreampoetlee@gmail.com>
2022-05-05 10:02:03 -0400

    Made minor changes to question 7

--- app.js
+++ app.js
@@ -230,16 +230,18 @@ console.log('Here are a list of dishes by their cuisine: ', cuisineNames);
 
 
 //7. Create a function that will append the cuisine type to the start of the dish's name. Ie, "Italian Pizza"
-//Map 
-function moveCuisineName(){
-	let results = dishes.map(function(el) {
-		return el.unshift('cuisine') + el.name
-	})
-	return results
-}
+//Map
+dishes.unshift('cuisine');
 
-let cuisineNameMoved = moveCuisineName();
-console.log(cuisineNameMoved);
+// function moveCuisineName(){
+// 	let results = dishes.map(function(el) {
+// 		return el.unshift('cuisine') + el.name
+// 	})
+// 	return results
+// }
+
+// let cuisineNameMoved = moveCuisineName();
+// console.log(cuisineNameMoved);
 
 //8. Create a function that will append the cuisine type to the start of the dish's name. Then, return only the Vegetarian dish objects. So this function should return objects 11-13 with their names changed to "Vegetarian Lasagna", "Vegetarian Falafel", "Vegetarian Chili"
 //Map, Filter
